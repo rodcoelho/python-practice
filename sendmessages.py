6f35652eb30c38ea04c9c999d58afc7193ebc8a3
rodcoelho <rod@coelhoinvestments.com>
2017-06-12 17:02:18 -0500

    sendmessages.py used in AirQuality project to send messages

---
+++ sendmessages.py
@@ -0,0 +1,17 @@
+import yagmail
+username = '############'
+password = '############'
+yag = yagmail.SMTP(username,password)
+providers = {'ATT':'txt.att.net', 'T-mobile':'tmomail.net', 'Verizon':'vtext.com', 'Sprint': 'messaging.sprintpcs.com', 'metroPCS':'metropcs.sms.us'}
+
+def sendtext(phone, provider, subject, body):
+    x = provider
+    phonemail = phone+'@'+providers[x]
+    sendemail(phonemail,subject,body)
+
+def sendemail(to,subject, body):
+    yagmail.SMTP(username).send(to, subject,body)
+
+def main():
+    if __name__ == "__main__":
+        main( )
