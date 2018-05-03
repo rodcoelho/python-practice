d6204232aa0f6772735b65154725e7cdb8c53021
rodcoelho <rodcpython@gmail.com>
2018-05-03 13:22:13 -0400

    simple warm up

---
+++ trouble.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+#
+# monkey_trouble(True, True) → True
+# monkey_trouble(False, False) → True
+# monkey_trouble(True, False) → False
+
+
+def monkey_trouble(a_smile, b_smile):
+    value = a_smile + b_smile
+    if value == 1:
+        return False
+    else:
+        return True
+
