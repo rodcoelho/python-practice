f0668b4b36c8e7667ab9f8cd59de4d87b449ac1d
rodcoelho <rodcpython@gmail.com>
2018-05-11 10:09:02 -0400

    find digit occurance in range - PEP

--- num_of_x_in_range.py
+++ num_of_x_in_range.py
@@ -2,6 +2,7 @@
 
 # Given an integer X within the range of 0 to 9, and given two positive integers as upper and lower bounds respectively, find the number of times X occurs as a digit in an integer within the range, excluding the bounds. Print the frequency of occurrence as output.
 
+
 def find_x_in_range(x, lower, upper):
     count = 0
     gen_x = (y for y in range(lower+1, upper))
@@ -18,3 +19,4 @@ if __name__ == '__main__':
     assert find_x_in_range(0, 20, 21) == 0
     assert find_x_in_range(9, 899, 1000) == 120
     assert find_x_in_range(1, 1100, 1345) == 398
+
