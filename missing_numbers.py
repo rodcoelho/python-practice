7cb3b83b70869f1929e209cb374174ff43feddc7
rodcoelho <rodcpython@gmail.com>
2018-05-28 16:00:56 -0400

    pre solution

---
+++ missing_numbers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# you are given an array of values that either count
+
+def missing_number(arr):
+
+
+if __name__ == '__main__':
+    assert missing_number([1,2,3,5,6,7]) == 4, 'error1'
