d97977766effa8433d4eb4f075130b65d270e7e5
rodcoelho <rodcpython@gmail.com>
2018-05-28 18:20:26 -0400

    simplest case

--- missing_numbers.py
+++ missing_numbers.py
@@ -3,7 +3,18 @@
 # you are given an array of values that either count
 
 def missing_number(arr):
+    answer = -1
+
+    for i in range(0, len(arr)-1):
+        if arr[i] + 1 == arr[i + 1]:
+            pass
+        else:
+            answer = arr[i] + 1
+
+    return answer
 
 
 if __name__ == '__main__':
     assert missing_number([1,2,3,5,6,7]) == 4, 'error1'
+
+
