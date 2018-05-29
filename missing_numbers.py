6843d273af4c2e604184ef82b44b439e98e422a0
rodcoelho <rodcpython@gmail.com>
2018-05-29 10:32:34 -0400

    pre-skips solution - error

--- missing_numbers.py
+++ missing_numbers.py
@@ -3,20 +3,31 @@
 # you are given an array of values that either count
 
 
-def missing_number(arr):
+def find_missing_num(arr):
     answer = -1
-
-    for i in range(0, len(arr)-1):
-        if arr[i] + 1 == arr[i + 1]:
-            pass
-        else:
-            answer = arr[i] + 1
-
+    skip = find_skip(arr)
+    print(skip)
+    for i in range(0, len(arr) - 1, skip):
+        if arr[i] + skip != arr[i + 1]:
+            answer = arr[i] + skip
     return answer
 
 
+def find_skip(arr):
+    skip = arr[1] - arr[0]
+    skip2 = arr[-1] - arr[-2]
+    if skip == skip2:
+        return skip
+    else:
+        skip3 = arr[-2] - arr[-3]
+        if skip == skip3:
+            return skip
+        else:
+            return arr[2] - arr[1]
+
+
 if __name__ == '__main__':
-    assert missing_number([1, 2, 3, 5, 6, 7]) == 4, 'error1'
+    assert find_missing_num([2, 4, 8, 10]) == 6, 'error1'
 
 # next we want to be able to do it for skips
 # then we want to be able to do it counting down
