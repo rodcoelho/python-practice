be19021890a57f6749beb55fbea541e0d363dd36
Rod Coelho <rodcpython@gmail.com>
2023-07-28 15:46:22 -0700

    search bar + filter by rooms

--- django/surfboard_spec_app/surfboard_spec/base/views.py
+++ django/surfboard_spec_app/surfboard_spec/base/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {"rooms": rooms}
+    # get the q from search in the navbar.html
+    q = request.GET.get("q") if request.GET.get("q") != None else ''
+
+    rooms = Room.objects.filter( # multiple queries using OR statement
+        Q(topic__name__icontains = q) |
+        Q(name__icontains = q) |
+        Q(description__icontains = q)
+        )
+    room_count = rooms.count()
+
+    topics = Topic.objects.all()
+
+    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+
     context = {"room": room}
     return render(request, 'base/room.html', context)
 
@@ -39,13 +52,13 @@ def updateRoom(request, pk):
             form.save()
             return redirect("home")
 
-
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
 
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+
     if request.method == 'POST':
         room.delete()
         return redirect("home")
