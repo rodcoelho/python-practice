1f52f2efecf59335f8e47a6df0f6b6b08bcd30f1
Rod Coelho <rodcpython@gmail.com>
2023-08-15 11:17:33 -0700

    edit profile

--- django/surfboard_spec_app/surfboard_spec/base/views.py
+++ django/surfboard_spec_app/surfboard_spec/base/views.py
@@ -220,5 +220,4 @@ def updateUser(request):
         if form.is_valid():
             form.save()  
             return redirect("user-profile", pk=user.id)
-
     return render(request, 'base/update_user.html', context)
