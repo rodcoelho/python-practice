6b42f26e7d1fe5f8ac671ffb6650eef737d1e7f9
Rod Coelho <rodcpython@gmail.com>
2023-08-21 15:11:56 -0700

    ex1_5

---
+++ python-mastery/stock.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
+    
