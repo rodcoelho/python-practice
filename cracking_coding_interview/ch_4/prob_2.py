b306fcc26b3bb796cb23d85bc501bd4d91c84028
Rod Coelho <rodcpython@gmail.com>
2023-09-18 17:09:21 +0000

    c4p2

---
+++ cracking_coding_interview/ch_4/prob_2.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import unittest
+from collections import deque
+
+# VISUAL OF TEST GRAPH:
+
+# A -- B
+# |    |
+# C -- D
+# |
+# E -- F -- G -- H
+#      | \
+#      O   I -- J -- K
+#               |
+#               L
+
+# P -- Q
+# |  /
+# R
+
+
+def is_route(graph, start, end, visited=None):
+
+    if visited == None:
+        visited = set()
+    
+    for node in graph[start]:
+
+        if node not in visited:
+            visited.add(node)
+
+            # recurse into the node by making the node as the new start
+            if node == end or is_route(graph, node, end, visited):
+                return True
+
+    return False
+
+
+class Test(unittest.TestCase):
+
+    graph = {
+        "A": ["B", "C"],
+        "B": ["D"],
+        "C": ["D", "E"],
+        "D": ["B", "C"],
+        "E": ["C", "F"],
+        "F": ["E", "O", "I", "G"],
+        "G": ["F", "H"],
+        "H": ["G"],
+        "I": ["F", "J"],
+        "O": ["F"],
+        "J": ["K", "L", "I"],
+        "K": ["J"],
+        "L": ["J"],
+        "P": ["Q", "R"],
+        "Q": ["P", "R"],
+        "R": ["P", "Q"],
+    }
+
+    tests = [
+        ("A", "L", True),
+        ("A", "B", True),
+        ("H", "K", True),
+        ("L", "D", True),
+        ("P", "Q", True),
+        ("Q", "P", True),
+        ("Q", "G", False),
+        ("R", "A", False),
+        ("P", "B", False),
+    ]
+
+    def test_is_route(self):
+        for [start, end, expected] in self.tests:
+            actual = is_route(self.graph, start, end)
+            
+            self.assertEqual(actual, expected, (actual, expected))
+
+
+if __name__ == "__main__":
+    unittest.main()
