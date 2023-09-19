9afea0d8f1349c59ca8008a79337de8d8aa0cc2f
Rod Coelho <rodcpython@gmail.com>
2023-09-19 18:42:34 +0000

    c4p2

--- cracking_coding_interview/ch_4/prob_2.py
+++ cracking_coding_interview/ch_4/prob_2.py
@@ -3,7 +3,10 @@
 import unittest
 from collections import deque
 
-# VISUAL OF TEST GRAPH:
+"""
+Given a graph, design a function to find out whether there is a route between two nodes.
+
+VISUAL OF TEST GRAPH
 
 # A -- B
 # |    |
@@ -18,61 +21,66 @@ from collections import deque
 # P -- Q
 # |  /
 # R
+"""
 
 
-def is_route(graph, start, end, visited=None):
+class MyGraph:
+    def __init__(self, graph):
+        self.graph = graph
 
-    if visited == None:
-        visited = set()
-    
-    for node in graph[start]:
+    def is_route(self, start, end, visited=None):
+        if visited == None:
+            visited = set()
+        
+        for node in self.graph[start]:
+            
+            if node not in visited:
+                visited.add(node)
 
-        if node not in visited:
-            visited.add(node)
-
-            # recurse into the node by making the node as the new start
-            if node == end or is_route(graph, node, end, visited):
-                return True
-
-    return False
+                # recurse into the node by making the node as the new start
+                if node == end or self.is_route(node, end, visited):
+                    return True
+        return False
 
 
-class Test(unittest.TestCase):
+class TestMyGraph(unittest.TestCase):
+    def setUp(self):
+        self.graph = {
+            "A": ["B", "C"],
+            "B": ["D"],
+            "C": ["D", "E"],
+            "D": ["B", "C"],
+            "E": ["C", "F"],
+            "F": ["E", "O", "I", "G"],
+            "G": ["F", "H"],
+            "H": ["G"],
+            "I": ["F", "J"],
+            "O": ["F"],
+            "J": ["K", "L", "I"],
+            "K": ["J"],
+            "L": ["J"],
+            "P": ["Q", "R"],
+            "Q": ["P", "R"],
+            "R": ["P", "Q"],
+        }
 
-    graph = {
-        "A": ["B", "C"],
-        "B": ["D"],
-        "C": ["D", "E"],
-        "D": ["B", "C"],
-        "E": ["C", "F"],
-        "F": ["E", "O", "I", "G"],
-        "G": ["F", "H"],
-        "H": ["G"],
-        "I": ["F", "J"],
-        "O": ["F"],
-        "J": ["K", "L", "I"],
-        "K": ["J"],
-        "L": ["J"],
-        "P": ["Q", "R"],
-        "Q": ["P", "R"],
-        "R": ["P", "Q"],
-    }
-
-    tests = [
-        ("A", "L", True),
-        ("A", "B", True),
-        ("H", "K", True),
-        ("L", "D", True),
-        ("P", "Q", True),
-        ("Q", "P", True),
-        ("Q", "G", False),
-        ("R", "A", False),
-        ("P", "B", False),
-    ]
+        self.test_cases = [
+            ("A", "L", True),
+            ("A", "B", True),
+            ("H", "K", True),
+            ("L", "D", True),
+            ("P", "Q", True),
+            ("Q", "P", True),
+            ("Q", "G", False),
+            ("R", "A", False),
+            ("P", "B", False),
+        ]
 
     def test_is_route(self):
-        for [start, end, expected] in self.tests:
-            actual = is_route(self.graph, start, end)
+        mg = MyGraph(self.graph)
+
+        for [start, end, expected] in self.test_cases:
+            actual = mg.is_route(start, end)
             
             self.assertEqual(actual, expected, (actual, expected))
 
