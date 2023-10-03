552da5a4c8b48a6007b2a3f4bc603ce0dc729bcb
Rod Coelho <rodcpython@gmail.com>
2023-10-03 18:13:34 +0000

    bad stock class

--- python-mastery/stock.py
+++ python-mastery/stock.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import csv
+from decimal import Decimal
 
 class Portfolio:
     def __init__(self, filename):
@@ -34,10 +35,10 @@ class Portfolio:
         for s in self.stocks:
             print('%10s %10s %10s' % (s.name, s.shares, s.price))
 
-            
 
 class Stock:
-    types = (str, int, float)
+    __slots__ = ('name','_shares','_price')
+    _types = (str, int, float)
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -45,23 +46,57 @@ class Stock:
 
     @classmethod
     def from_row(cls, row):
-        vals = (func(val) for func, val in zip(cls.types, row))
-        return cls(*vals)
+        values = [func(val) for func, val in zip(cls._types, row)]
+        return cls(*values)
 
+    @property
+    def shares(self):
+        return self._shares
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, self._types[1]):
+            raise TypeError(f'Expected {self._types[1].__name__}')
+        if value < 0:
+            raise ValueError('shares must be >= 0')
+        self._shares = value
+
+    @property
+    def price(self):
+        return self._price
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, self._types[2]):
+            raise TypeError(f'Expected {self._types[2].__name__}')
+        if value < 0:
+            raise ValueError('price must be >= 0')
+        self._price = value
+
+    @property
     def cost(self):
         return self.shares * self.price
 
-    def sell(self, quantity):
-        if quantity <= self.shares:
-            self.shares -= quantity
+    def sell(self, nshares):
+        self.shares -= nshares
+
+class DecimalStock(Stock):
+    _types = (str, int, Decimal)
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
 
 if __name__ == "__main__":
     portfolio = Portfolio("Data/portfolio.csv")
     portfolio.print_portfolio()
 
-    s = Stock.from_row(['AA', '100', '75'])
-    print(s.cost())
+    s = Stock.from_row(['AA', 100, 75.0])
+    print(s.cost)
     
-    
+    s = Stock("GOOG", 10, 100.0)
+    print(s.cost)
+
+    s = DecimalStock("AAPL", 100, Decimal(101.0))
+    print(s.cost)
 
     
