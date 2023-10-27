ea19be982d138ac202e218a78cccaab147e3aea9
Rod Coelho <rodcpython@gmail.com>
2023-10-27 16:13:18 +0000

    overlapping days

--- archived_problems/human_interest_date_ranges.py
+++ archived_problems/human_interest_date_ranges.py
@@ -28,8 +28,27 @@ class Solution:
                 current_date += timedelta(days=1)
             
         return len(unique_days)
-        
 
+    def overlap_schedules(self, schedules):
+        schedule_map = {}
+
+        for s in schedules:
+            name, beg, end = s
+            beg_yyyy, beg_mm, beg_dd = beg.split("-")
+            end_yyyy, end_mm, end_dd = end.split("-")
+            beg_date = date(int(beg_yyyy), int(beg_mm), int(beg_dd))
+            end_date = date(int(end_yyyy), int(end_mm), int(end_dd))
+
+            current = beg_date
+            while current <= end_date:
+                if current not in schedule_map:
+                    schedule_map[current] = set()
+                schedule_map[current].add(name)
+                current += timedelta(days=1)
+
+        return len([key for key, value in schedule_map.items() if len(value) > 1])
+
+    
 class TestSolution(unittest.TestCase):
     def test_count_unique_days_datetime(self):
         s = Solution()
@@ -54,5 +73,20 @@ class TestSolution(unittest.TestCase):
         expected = 11
         actual = s.count_unique_days_string(ranges)
         self.assertEqual(actual, expected)
+
+    def test_overlapping_days(self):
+        s = Solution()
+
+        schedules = [
+            ("bob", "2020-1-1", "2020-1-10"),
+            ("reese", "2020-1-4", "2020-1-15"),
+            ("bob", "2020-1-16", "2020-2-3"),
+            ("reese", "2020-2-1", "2020-2-9"),
+            ("bob", "2020-2-6", "2020-2-8")
+        ]
+        expected = 13
+        actual = s.overlap_schedules(schedules)
+        self.assertEqual(actual, expected)
+        
 
 unittest.main()
